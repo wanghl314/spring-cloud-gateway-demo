cc71d11798728d57e36bbf1b9599df96f77984fc
王洪亮 <w505957125@aliyun.com>
2024-03-07 15:11:55 +0800

    1. add ServerHttpRequest ServerHttpResponse

--- src/main/java/com/weaver/emobile/gateway/controller/BaseController.java
+++ src/main/java/com/weaver/emobile/gateway/controller/BaseController.java
@@ -1,20 +1,21 @@
 package com.weaver.emobile.gateway.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.weaver.emobile.gateway.util.Consts;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.weaver.emobile.gateway.util.Consts;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/base")
 public class BaseController {
 
     @GetMapping("/getsetting")
-    public Map<String, String> getSetting() {
+    public Map<String, String> getSetting(ServerHttpRequest request, ServerHttpResponse response) {
         Map<String, String> data = new HashMap<String, String>();
         data.put("keyEncryptType", "RSA");
         data.put("dataEncryptType", "AES");
